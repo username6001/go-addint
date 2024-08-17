d73552367c109c564671859ef02c281f81cfe62b
username6001 <username6001@yandex.ru>
2024-08-17 14:30:03 +0400

    add godoc

--- addint.go
+++ addint.go
@@ -1,5 +1,14 @@
+// addint package provides ability to add two integers.
+//
+// This is a simple package that provides a function to add two integers.
+// https://www.mathsisfun.com/numbers/addition.html
 package addint
 
+// AddInt returns the sum of two integers.
+//
+// Example:
+//
+//	fmt.Println(AddInt(1, 2)) // Output: 3
 func AddInt(a, b int) int {
 	return a + b
 }
