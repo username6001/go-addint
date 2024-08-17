f4262bba1be3f5ee4f277700cc5438db45a79106
username6001 <username6001@yandex.ru>
2024-08-17 15:02:02 +0400

    addint.go: AddInt accepts Integer and Float

--- addint.go
+++ addint.go
@@ -4,11 +4,17 @@
 // https://www.mathsisfun.com/numbers/addition.html
 package addint
 
+import "golang.org/x/exp/constraints"
+
+type Number interface {
+	constraints.Integer | constraints.Float
+}
+
 // AddInt returns the sum of two integers.
 //
 // Example:
 //
 //	fmt.Println(AddInt(1, 2)) // Output: 3
-func AddInt(a, b int) int {
+func AddInt[T Number](a, b T) T {
 	return a + b
 }
--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/username6001/go-addint
 
 go 1.22.6
+
+require golang.org/x/exp v0.0.0-20240808152545-0cdaa3abc0fa
